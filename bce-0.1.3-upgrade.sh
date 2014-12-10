4d7c7821b35a6ec04854e64029d85b7324bee667
Aaron Culich <aculich@berkeley.edu>
2014-12-09 17:25:05 -0800

    install modules as root

--- bce-0.1.3-upgrade.sh
+++ bce-0.1.3-upgrade.sh
@@ -5,8 +5,9 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 sudo apt-get update && sudo apt-get install -y r-recommended libgsl0-dev
 wget http://cran.r-project.org/src/contrib/Archive/tm/tm_0.5-10.tar.gz
 wget http://cran.r-project.org/src/contrib/Archive/topicmodels/topicmodels_0.2-0.tar.gz
-R CMD INSTALL tm_0.5-10.tar.gz
-R CMD INSTALL topicmodels_0.2-0.tar.gz
+sudo rm -rf /usr/local/lib/R/site-library/tm /usr/local/lib/R/site-library/topicmodels
+sudo R CMD INSTALL tm_0.5-10.tar.gz
+sudo R CMD INSTALL topicmodels_0.2-0.tar.gz
 sudo R --vanilla <<EOF
 repos <- 'http://cran.cnr.berkeley.edu'
 pkgs <- c("SnowballC", "slam")
