84779443a74a5b37e5268566bdef5ab1270cd017
Aaron Culich <aculich@berkeley.edu>
2014-12-09 17:47:51 -0800

    disable chrome updates for now

--- bce-0.1.3-upgrade.sh
+++ bce-0.1.3-upgrade.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" | sudo tee /etc/apt/sources.list.d/cran-rstudio-trusty.list
+sudo mv /etc/apt/sources.list.d/google-chrome.list /etc/apt/sources.list.d/.google-chrome.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 sudo apt-get update && sudo apt-get install -y r-recommended libgsl0-dev
 sudo rm -rf /usr/local/lib/R/site-library/tm /usr/local/lib/R/site-library/topicmodels
